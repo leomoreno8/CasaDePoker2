da0558e475bafb91f4710431726f82417c6e7dc3
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2022-05-27 19:11:25 -0300

    Initial commit

---
+++ src/main/java/com/leo/casadepoker2/CasaDePoker2Application.java
@@ -0,0 +1,13 @@
+package com.leo.casadepoker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDePoker2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasaDePoker2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/leo/casadepoker2/CasaDePoker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.casadepoker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDePoker2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
